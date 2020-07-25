df136af931a71edb899c1b098355bf73f68c7886
Subham <das.jishu25@gmail.com>
2020-07-25 08:35:50 +0530

    Added new methods

---
+++ src/__tests__/array.test.ts
@@ -0,0 +1,38 @@
+import { shuffle, removeDuplicates, countItem, countAll, pickRandom } from '../index';
+
+test('TEST - Shuffling Array', () => {
+  let arr = [2, 7, 9, 0, 1, 5];
+  let shuffledarr = shuffle(arr);
+  expect(arr.length).toBe(shuffledarr.length);
+});
+
+test('TEST - Removing duplicates from Array', () => {
+  let arr = [2, 2, 9, 4, 4, 5, 9, 2];
+  expect(removeDuplicates(arr)).toStrictEqual([2, 9, 4, 5]);
+});
+
+test('TEST:1 - Picking random from Array', () => {
+  let arr = [2, 7, 9, 0, 1, 5];
+  let randomArr = pickRandom(arr, 5, false);
+  expect(randomArr.length).toBe(5);
+});
+
+test('TEST:2 - Picking random from Array', () => {
+  let arr = [2, 7, 9, 0, 1, 5];
+  let randomArr = pickRandom(arr, 6, true);
+  expect(randomArr.length).toBe(6);
+});
+
+test('TEST - Count item in an Array', () => {
+  let arr = [2, 7, 9, 7, 1, 5];
+  expect(countItem(arr, 7)).toBe(2);
+});
+
+test('TEST - Count all items in an Array', () => {
+  let arr = [2, 7, 9, 7, 7, 2];
+  let result = new Map();
+  result.set(2, 2);
+  result.set(7, 3);
+  result.set(9, 1);
+  expect(countAll(arr)).toStrictEqual(result);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 
 //ARRAY UTILITIES
+
+//Shuffling array
 export const shuffle = (array: Array<any>) => {
   let currentIndex = array.length, temp, randomIndex;
 
@@ -12,4 +14,55 @@ export const shuffle = (array: Array<any>) => {
     array[randomIndex] = temp;
   }
   return array;
+};
+
+//Remove duplicates
+export const removeDuplicates = (array: Array<any>) => {
+  let arr: Array<any> = [];
+  array.forEach(item => {
+    if(!arr.includes(item))
+    arr.push(item);
+  });
+  return arr;
+};
+
+//Picking random elements
+export const pickRandom = (array: Array<any>, n: Number, unique: Boolean) => {
+  let arr: Array<any> = shuffle(array);
+  if (unique === true) {
+    arr = removeDuplicates(arr);
+  }
+
+  let rand: Array<any> = [];
+  let i = 0, k = 0;
+  while (n > k) {
+    if(i >= arr.length) {
+      arr = shuffle(arr);
+      i = 0;
+    }
+    rand.push(arr[i]);
+    i++;
+    k++;
+  }
+  return rand;
+};
+
+//Count an element in the array
+export const countItem = (array: Array<any>, item: any) => {
+  let c = 0;
+  array.forEach(x => {
+    if(x === item)
+    c++;
+  });
+  return c;
+};
+
+//Frequency of each item in the array
+export const countAll = (array: Array<any>) => {
+  let result = new Map();
+  let arr = removeDuplicates(array);
+  arr.forEach(item => {
+    result.set(item, countItem(array, item));
+  });
+  return result;
 };
